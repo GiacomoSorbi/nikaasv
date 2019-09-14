a0485791824c102a4dfdd5df8320ad44d89ae0d6
Veronika Savikina <verosja@gmail.com>
2019-09-14 22:56:03 +0100

    Updates

---
+++ precache-manifest.b99ad2e8c85fc9914b3b10232e67a679.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747ed468307bd283f718761d04781176",
+    "url": "/nikaasv/index.html"
+  },
+  {
+    "revision": "8497cf859a34991bf419",
+    "url": "/nikaasv/static/css/main.fae0880a.chunk.css"
+  },
+  {
+    "revision": "a613fb95ede60782c3d2",
+    "url": "/nikaasv/static/js/2.715a97d7.chunk.js"
+  },
+  {
+    "revision": "8497cf859a34991bf419",
+    "url": "/nikaasv/static/js/main.c397a299.chunk.js"
+  },
+  {
+    "revision": "35ef947bb6b68ae0c0ec",
+    "url": "/nikaasv/static/js/runtime~main.2217af68.js"
+  },
+  {
+    "revision": "ba70e136cd84529620646852e459413c",
+    "url": "/nikaasv/static/media/Baskerville.ba70e136.ttc"
+  },
+  {
+    "revision": "fd41d56cdae2b643a6f1b1faa34cceb8",
+    "url": "/nikaasv/static/media/Copperplate.fd41d56c.ttc"
+  },
+  {
+    "revision": "f16616a95e83968bfcf71780c40c395f",
+    "url": "/nikaasv/static/media/Sacramento-Regular.f16616a9.ttf"
+  },
+  {
+    "revision": "77a89b3fc64faba05ccd3d8526e2cde7",
+    "url": "/nikaasv/static/media/about-us.77a89b3f.jpg"
+  },
+  {
+    "revision": "0b74ecb6c4228caa34f97d88490abbea",
+    "url": "/nikaasv/static/media/circle.0b74ecb6.jpg"
+  },
+  {
+    "revision": "bd5b625b05d2035dcd724a8609099bcb",
+    "url": "/nikaasv/static/media/ear.bd5b625b.jpg"
+  },
+  {
+    "revision": "e029ad0a22c170668b833aa74c53986c",
+    "url": "/nikaasv/static/media/girl.e029ad0a.jpg"
+  },
+  {
+    "revision": "cff782e5c20e43461473f79a200e11d0",
+    "url": "/nikaasv/static/media/goldenChain.cff782e5.jpg"
+  },
+  {
+    "revision": "74e1987d357d28c300e722e2d89776c1",
+    "url": "/nikaasv/static/media/goldenRing.74e1987d.jpg"
+  },
+  {
+    "revision": "966636dde08e375e14d82a47a579be69",
+    "url": "/nikaasv/static/media/neck.966636dd.jpg"
+  },
+  {
+    "revision": "b8308190f9f287f4f707e06a82a12c21",
+    "url": "/nikaasv/static/media/oval.b8308190.jpg"
+  },
+  {
+    "revision": "add1d1b17b4ff4ee681ce31555545eb8",
+    "url": "/nikaasv/static/media/silverChain.add1d1b1.jpg"
+  },
+  {
+    "revision": "cc47e47fedad314f1ee935c687618e46",
+    "url": "/nikaasv/static/media/silverRing.cc47e47f.jpg"
+  },
+  {
+    "revision": "4ab460374057f91542847d101a2425e4",
+    "url": "/nikaasv/static/media/soloGold.4ab46037.jpg"
+  },
+  {
+    "revision": "4dca3fab2ff33a6fcbfc8474af0b77ba",
+    "url": "/nikaasv/static/media/stone.4dca3fab.jpg"
+  },
+  {
+    "revision": "83c59de23181e54fccf5dc608f0d1cfd",
+    "url": "/nikaasv/static/media/twoGolden.83c59de2.jpg"
+  }
+]);
